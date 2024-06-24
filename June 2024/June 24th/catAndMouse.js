d2480a50e77571cef45c1708ac90b128846eee8f
Grace Clower <clower.grace@gmail.com>
2024-06-24 09:08:27 -0400

    daily js and py practice

---
+++ June 2024/June 24th/catAndMouse.js
@@ -0,0 +1,39 @@
+// Two cats and a mouse are at various positions on a line. You will be given their starting positions. Your task is to determine which cat will reach the mouse first, assuming the mouse does not move and the cats travel at equal speed. If the cats arrive at the same time, the mouse will be allowed to move and it will escape while they fight.
+
+// You are given  queries in the form of , , and  representing the respective positions for cats  and , and for mouse . Complete the function  to return the appropriate answer to each query, which will be printed on a new line.
+
+// If cat  catches the mouse first, print Cat A.
+// If cat  catches the mouse first, print Cat B.
+// If both cats reach the mouse at the same time, print Mouse C as the two cats fight and mouse escapes.
+// Example
+
+
+
+
+// The cats are at positions  (Cat A) and  (Cat B), and the mouse is at position . Cat B, at position  will arrive first since it is only  unit away while the other is  units away. Return 'Cat B'.
+
+// Function Description
+
+// Complete the catAndMouse function in the editor below.
+
+// catAndMouse has the following parameter(s):
+
+// int x: Cat 's position
+// int y: Cat 's position
+// int z: Mouse 's position
+// Returns
+
+// string: Either 'Cat A', 'Cat B', or 'Mouse C'
+
+function catAndMouse(catA, catB, mouseC) {
+    let atoc = Math.abs(mouseC-catA)
+    let btoc = Math.abs(mouseC-catB)
+    
+    if(atoc==btoc){
+      return "Mouse C"
+    }else if(atoc>btoc){
+      return "Cat B"
+    }else{
+      return "Cat A"
+    }
+  }
