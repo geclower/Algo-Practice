218f2d45e143e52f8d0e52c7309332d55fb53f22
Grace Clower <clower.grace@gmail.com>
2024-07-16 10:25:08 -0400

    py practice

--- July 2024/July 16th/circularRotation.py
+++ July 2024/July 16th/circularRotation.py
@@ -23,3 +23,19 @@
 # Returns
 
 # int[q]: the values in the rotated  as requested in 
+
+def circulatArrayRotation(arr, k, queries):
+    result = []
+    i = 0
+
+    while i < k :
+        temp = arr.pop()
+        arr.insert(0, temp)
+        i += 1
+
+    for i in queries:
+        result.append(arr[queries[i]])
+    
+    return result
+
+print(circulatArrayRotation([39356, 87674, 16667, 54260, 43958, 70429, 53682, 6169, 87496, 66190, 90286, 4912, 44792, 65142, 36183, 43856, 77633, 38902, 1407, 88185, 80399, 72940, 97555, 23941, 96271, 49288, 27021, 32032, 75662, 69161, 33581, 15017, 56835, 66599, 69277, 17144, 37027, 39310, 23312, 24523, 5499, 13597, 45786, 66642, 95090, 98320, 26849, 72722, 37221, 28255, 60906], 2,[0]))
