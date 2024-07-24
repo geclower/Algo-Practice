feceb257d6b0499e8d9eced3c88df0ba65ccefdc
Grace Clower <clower.grace@gmail.com>
2024-07-24 10:55:58 -0400

    php praactice

--- July 2024/July 24th/findDigits.php
+++ July 2024/July 24th/findDigits.php
@@ -1,24 +1,42 @@
-<!-- An integer  is a divisor of an integer  if the remainder of .
+<?php
 
-Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.
+// <!-- An integer  is a divisor of an integer  if the remainder of .
 
-Example
+// Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.
 
-Check whether ,  and  are divisors of . All 3 numbers divide evenly into  so return .
+// Example
+
+// Check whether ,  and  are divisors of . All 3 numbers divide evenly into  so return .
 
 
-Check whether , , and  are divisors of . All 3 numbers divide evenly into  so return .
+// Check whether , , and  are divisors of . All 3 numbers divide evenly into  so return .
 
 
-Check whether  and  are divisors of .  is, but  is not. Return .
+// Check whether  and  are divisors of .  is, but  is not. Return .
 
-Function Description
+// Function Description
 
-Complete the findDigits function in the editor below.
+// Complete the findDigits function in the editor below.
 
-findDigits has the following parameter(s):
+// findDigits has the following parameter(s):
 
-int n: the value to analyze
-Returns
+// int n: the value to analyze
+// Returns
 
-int: the number of digits in  that are divisors of  -->
+// int: the number of digits in  that are divisors of  -->
+
+
+function findDigits($n){
+    $result = 0;
+    $arr = array_map('intval', str_split($n));
+
+    for($i = 0; $i<count($arr); $i++){
+        if($n%$arr[$i]===0){
+            $result++;
+        }
+    }
+
+    return $result;
+}
+
+echo findDigits(12);
