d3287001a9d1734b61d4c79029d57eb42fc412d7
Grace Clower <clower.grace@gmail.com>
2024-08-14 09:51:15 -0400

    js practice

---
+++ August 2024/August 13/August 14/libraryFine.js
@@ -0,0 +1,19 @@
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+  let fine = 0;
+
+  if (y1 > y2) {
+    fine = 10000;
+    return fine;
+  } else if (y1 < y2) {
+    return fine;
+  } else if (m1 > m2) {
+    fine = 500 * (m1 - m2);
+    return fine;
+  } else if (m1 < m2) {
+    return fine;
+  } else if (d1 > d2) {
+    fine = 15 * (d1 - d2);
+    return fine;
+  }
+  return fine;
+}
