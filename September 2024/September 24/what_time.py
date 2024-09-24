489e32292ade8994eac5840470344c3f13f77c4f
Grace Clower <clower.grace@gmail.com>
2024-09-24 09:57:13 -0400

    py practice

---
+++ September 2024/September 24/what_time.py
@@ -0,0 +1,59 @@
+def time_in_words(h, m):
+    mins = [ "o' clock",
+    "one minute",
+    "two minutes",
+    "three minutes",
+    "four minutes",
+    "five minutes",
+    "six minutes",
+    "seven minutes",
+    "eight minutes",
+    "nine minutes",
+    "ten minutes",
+    "eleven minutes",
+    "twelve minutes",
+    "thirteen minutes",
+    "fourteen minutes",
+    "quarter",
+    "sixteen minutes",
+    "seventeen minutes",
+    "eighteen minutes",
+    "ninteen minutes",
+    "twenty minutes",
+    "twenty one minutes",
+    "twenty two minutes",
+    "twenty three minutes",
+    "twenty four minutes",
+    "twenty five minutes",
+    "twenty six minutes",
+    "twenty seven minutes",
+    "twenty eight minutes",
+    "twenty nine minutes",
+    "half"]
+    hrs = [
+         "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve"
+    ]
+    answer = ""
+
+    if m == 0:
+        answer = hrs[h] + " " + mins[m]
+    if m <= 30 and m > 0:
+        answer = mins[m] + " past " + hrs[h]
+    if m > 30:
+        answer = mins[(60-m)] + " to " + hrs[h+1]
+    
+    return answer
+
+print(time_in_words(5, 47))
